bf35f795db49200f24cbaf73425359203a1f20af
Dadis <139597407+D4dis@users.noreply.github.com>
2024-09-10 17:00:51 +0200

    Add files via upload

--- views/groupe.php
+++ views/groupe.php
@@ -11,11 +11,34 @@ if (isset($_GET) && empty($_GET)) {
 }
 
 $groupe_id = $_GET['groupe'] ?? '';
+$tunnel_update = $_POST['tunnelEdit'] ?? '';
+$taverne_update = $_POST['taverneEdit'] ?? '';
 
 $dsn = DB_ENGINE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET;
 
 try {
   $pdo = new PDO(DSN, DB_USER, DB_PWD, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+  if (isset($_POST['tunnelEdit']) && !empty($_POST['tunnelEdit'])) {
+    $requestUpdate = $pdo->prepare('UPDATE groupe 
+                                  SET g_tunnel_fk = :tunnel_update
+                                  WHERE g_id = :groupe_id');
+    $requestUpdate->bindValue('groupe_id', $groupe_id);
+    $requestUpdate->bindValue('tunnel_update', $tunnel_update);
+    $requestUpdate->execute();
+    $requestUpdate->closeCursor();
+  }
+
+  if (isset($_POST['taverneEdit']) && !empty($_POST['taverneEdit'])) {
+
+    $requestUpdate = $pdo->prepare('UPDATE groupe 
+                                  SET g_taverne_fk = :taverne_update
+                                  WHERE g_id = :groupe_id');
+    $requestUpdate->bindValue('groupe_id', $groupe_id);
+    $requestUpdate->bindValue('taverne_update', $taverne_update);
+    $requestUpdate->execute();
+    $requestUpdate->closeCursor();
+  }
 
   $request = $pdo->prepare('SELECT g_id, t_nom, v_depart.v_nom v_depart, v_depart.v_id v_departid, v_arrive.v_id v_arriveid, v_arrive.v_nom v_arrive, g_debuttravail, g_fintravail, t_progres
                             FROM groupe
@@ -37,16 +60,28 @@ try {
   $nains = $requestNain->fetchAll(PDO::FETCH_ASSOC);
   $requestNain->closeCursor();
 
-  $requestInfoUpdate = $pdo->prepare('SELECT g_id, tunnel.t_id tun_id, t_nom, taverne.t_id tav_id
-                                FROM groupe
-                                JOIN tunnel ON groupe.g_tunnel_fk = tunnel.t_id
-                                JOIN taverne ON groupe.g_taverne_fk = taverne.t_id');
-  $requestInfoUpdate->execute();
-  $InfosUpdate = $requestInfoUpdate->fetchAll(PDO::FETCH_ASSOC);
-  $requestInfoUpdate->closeCursor();
+  $requestGUpdate = $pdo->prepare('SELECT g_id, g_debuttravail, g_fintravail
+                                FROM groupe');
+  $requestGUpdate->execute();
+  $GUpdate = $requestGUpdate->fetchAll(PDO::FETCH_ASSOC);
+  $requestGUpdate->closeCursor();
+
+  $requestTunUpdate = $pdo->prepare('SELECT t_id
+                                FROM tunnel');
+  $requestTunUpdate->execute();
+  $TunUpdate = $requestTunUpdate->fetchAll(PDO::FETCH_ASSOC);
+  $requestTunUpdate->closeCursor();
+
+  $requestTavUpdate = $pdo->prepare('SELECT t_id, t_nom
+                                FROM taverne');
+  $requestTavUpdate->execute();
+  $TavUpdate = $requestTavUpdate->fetchAll(PDO::FETCH_ASSOC);
+  $requestTavUpdate->closeCursor();
 } catch (PDOException $e) {
   die($e->getMessage());
 }
+
+asort($TunUpdate);
 
 var_dump($_POST);
 
@@ -84,7 +119,7 @@ var_dump($_POST);
   <?php if (!empty($groupe['g_debuttravail'])) : ?>
     <p class="text-center display-5">Travail de <?= $groupe['g_debuttravail'] ?> à <?= $groupe['g_fintravail'] ?> dans le tunnel <a href="ville.php?ville=<?= $groupe['v_departid'] ?>"><?= $groupe['v_depart'] ?></a> à <a href="ville.php?ville=<?= $groupe['v_arriveid'] ?>"><?= $groupe['v_arrive'] ?></a> (<?= $groupe['t_progres'] == 100 ? 'Entretiennent' : $groupe['t_progres'] . ' %' ?>)</p>
   <?php endif; ?>
-  
+
   <form class="d-flex justify-content-center" action="" method="post">
     <!-- Button trigger modal -->
     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2">
@@ -100,22 +135,22 @@ var_dump($_POST);
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            <select class="form-select form-select-sm mb-3" aria-label="Small select example" name="groupeEdit">
-              <option selected disabled>Changer le groupe</option>
-              <?php foreach ($groupes as $groupe) : ?>
-                <option value="<?= $groupe['g_id'] ?>"><?= $groupe['g_id'] ?></option>
+            <!-- <select class="form-select form-select-sm mb-3" aria-label="Small select example" name="groupeEdit">
+              <option selected disabled>Changer horaire</option>
+              <?php foreach ($GUpdate as $groupes) : ?>
+                <option value="<?= $groupes['g_id'] ?>">Groupe n°<?= $groupes['g_id'] ?></option>
               <?php endforeach; ?>
-            </select>
+            </select> -->
             <select class="form-select form-select-sm mb-3" aria-label="Small select example" name="tunnelEdit">
               <option selected disabled>Changer le tunnel</option>
-              <?php foreach ($groupes as $groupe) : ?>
-                <option value="<?= $groupe['g_id'] ?>"><?= $groupe['g_id'] ?></option>
+              <?php foreach ($TunUpdate as $tunnel) : ?>
+                <option value="<?= $tunnel['t_id'] ?>">Tunnel n°<?= $tunnel['t_id'] ?></option>
               <?php endforeach; ?>
             </select>
             <select class="form-select form-select-sm mb-3" aria-label="Small select example" name="taverneEdit">
               <option selected disabled>Changer la taverne</option>
-              <?php foreach ($groupes as $groupe) : ?>
-                <option value="<?= $groupe['g_id'] ?>"><?= $groupe['g_id'] ?></option>
+              <?php foreach ($TavUpdate as $taverne) : ?>
+                <option value="<?= $taverne['t_id'] ?>"><?= $taverne['t_nom'] ?></option>
               <?php endforeach; ?>
             </select>
           </div>
--- views/nain.php
+++ views/nain.php
@@ -18,14 +18,22 @@ $dsn = DB_ENGINE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_
 try {
   $pdo = new PDO(DSN, DB_USER, DB_PWD, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-  if (isset($_POST['groupeEdit']) && !empty($_POST['groupeEdit'])) {
+  if (isset($_POST['groupeEdit']) && !empty($_POST['groupeEdit']) && is_numeric($_POST['groupeEdit'])) {
     $requestUpdate = $pdo->prepare('UPDATE nain SET n_groupe_fk = :groupeEdit WHERE n_id = :nain_id');
     $requestUpdate->bindValue('nain_id', $nain_id);
     $requestUpdate->bindValue('groupeEdit', $groupeEdit);
     $requestUpdate->execute();
-    $groupes = $requestUpdate->fetchAll(PDO::FETCH_ASSOC);
     $requestUpdate->closeCursor();
   }
+
+  if (isset($_POST['groupeEdit']) && $_POST['groupeEdit'] == "NULL") {
+    $requestUpdate = $pdo->prepare('UPDATE nain SET n_groupe_fk = NULL WHERE n_id = :nain_id');
+    $requestUpdate->bindValue('nain_id', $nain_id);
+    $requestUpdate->execute();
+    $requestUpdate->closeCursor();
+  }
+
+
 
   $request = $pdo->prepare('SELECT n_nom, n_barbe, v_depart.v_nom v_depart, v_depart.v_id v_departid, v_arrive.v_nom v_arrive, v_arrive.v_id v_arriveid, t_nom, taverne.t_id tav_id, g_debuttravail, g_fintravail , g_id, v_natale.v_nom v_natale, v_natale.v_id v_nataleid
                             FROM nain
@@ -51,57 +59,56 @@ try {
 
 asort($groupes);
 
+var_dump($_POST);
 ?>
 
 <div class="container">
   <h1 class="text-center display-2 mb-5">Nain : <?= $nain['n_nom'] ?></h1>
-  <div class="d-flex flex-column align-items-center gap-5">
-    <div class="card border-0 shadow-sm" style="width: 20rem;">
-      <div class="card-body">
-        <p class="card-title">Taille barbe <?= $nain['n_barbe'] ?> cm</p>
-        <p class="card-title">Originaire de <a href="ville.php?ville=<?= $nain['v_nataleid'] ?>"><?= $nain['v_natale'] ?></a></p>
-        <?php if (!empty($nain['t_nom'])) : ?>
-          <p class="card-title">Boit dans <a href="taverne.php?taverne=<?= $nain['tav_id'] ?>"><?= $nain['t_nom'] ?></a></p>
-        <?php endif; ?>
-        <?php if (!empty($nain['g_debuttravail'])) : ?>
-          <p class="card-title">Travail de <?= $nain['g_debuttravail'] ?> à <?= $nain['g_fintravail'] ?> dans le tunnel <a href="ville.php?ville=<?= $nain['v_departid'] ?>"><?= $nain['v_depart'] ?></a> à <a href="ville.php?ville=<?= $nain['v_arriveid'] ?>"><?= $nain['v_arrive'] ?></a></p>
-        <?php endif; ?>
-        <?php if (!empty($nain['g_id'])) : ?>
-          <p class="card-title">Membre du <a href="groupe.php?groupe=<?= $nain['g_id'] ?>">groupe n° <?= $nain['g_id'] ?></a></p>
-        <?php else : ?>
-          <p class="card-title">Aucun groupe</p>
-        <?php endif; ?>
-      </div>
-    </div>
-    <form action="" method="post">
-      <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        Changer de groupe
-      </button>
+  <p class="text-center display-5">Taille barbe <?= $nain['n_barbe'] ?> cm</p>
+  <p class="text-center display-5">Originaire de <a href="ville.php?ville=<?= $nain['v_nataleid'] ?>"><?= $nain['v_natale'] ?></a></p>
+  <?php if (!empty($nain['t_nom'])) : ?>
+    <p class="text-center display-5">Boit dans <a href="taverne.php?taverne=<?= $nain['tav_id'] ?>"><?= $nain['t_nom'] ?></a></p>
+  <?php endif; ?>
+  <?php if (!empty($nain['g_debuttravail'])) : ?>
+    <p class="text-center display-5">Travail de <?= $nain['g_debuttravail'] ?> à <?= $nain['g_fintravail'] ?> dans le tunnel <a href="ville.php?ville=<?= $nain['v_departid'] ?>"><?= $nain['v_depart'] ?></a> à <a href="ville.php?ville=<?= $nain['v_arriveid'] ?>"><?= $nain['v_arrive'] ?></a></p>
+  <?php endif; ?>
+  <?php if (!empty($nain['g_id'])) : ?>
+    <p class="text-center display-5">Membre du <a href="groupe.php?groupe=<?= $nain['g_id'] ?>">groupe n° <?= $nain['g_id'] ?></a></p>
+  <?php else : ?>
+    <p class="text-center display-5">Aucun groupe</p>
+  <?php endif; ?>
+</div>
+</div>
+<form class="d-flex justify-content-center" action="" method="post">
+  <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Changer de groupe
+  </button>
 
-      <!-- Modal -->
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Changer le groupe</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <select class="form-select form-select-sm mb-3" aria-label="Small select example" name="groupeEdit">
-                <option selected disabled>Changer le groupe</option>
-                <?php foreach ($groupes as $groupe) : ?>
-                  <option value="<?= $groupe['g_id'] ?>"><?= $groupe['g_id'] ?></option>
-                <?php endforeach; ?>
-              </select>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
-              <button type="submit" class="btn btn-primary">Sauvegarder</button>
-            </div>
-          </div>
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Changer le groupe</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <select class="form-select form-select-sm mb-3" aria-label="Small select example" name="groupeEdit">
+            <option selected disabled>Changer le groupe</option>
+            <?php foreach ($groupes as $groupe) : ?>
+              <option value="<?= $groupe['g_id'] ?>" <?= $groupe['g_id'] == $nain['g_id'] ? 'selected' : '' ?>>Groupe n°<?= $groupe['g_id'] ?></option>
+            <?php endforeach; ?>
+            <option value="NULL">Pas de groupe</option>
+          </select>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+          <button type="submit" class="btn btn-primary">Sauvegarder</button>
         </div>
       </div>
-    </form>
+    </div>
   </div>
+</form>
+</div>
 </div>
