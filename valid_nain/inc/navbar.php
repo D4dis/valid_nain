06056c186017dc5b73f6520e153882a872c22e7d
Dadis <139597407+D4dis@users.noreply.github.com>
2024-09-10 11:29:49 +0200

    Delete valid_nain directory

--- valid_nain/inc/navbar.php
+++
@@ -1,6 +0,0 @@
-
-<nav class="navbar bg-body-tertiary shadow-sm">
-  <div class="container">
-    <a class="navbar-brand" href="../index.php"><i class="bi bi-house-door-fill"></i> Acceuil</a>
-  </div>
-</nav>
